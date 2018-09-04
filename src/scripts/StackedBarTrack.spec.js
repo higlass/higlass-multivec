be61f3e64c3e45719943e8858e700f8c2260e00d
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-09-04 17:08:31 -0400

    Started adding test files

---
+++ src/scripts/StackedBarTrack.spec.js
@@ -0,0 +1,166 @@
+import {
+  configure,
+  mount,
+  // render,
+  ReactWrapper,
+} from 'enzyme';
+
+const viewconf = {
+  "editable": true,
+  "zoomFixed": false,
+  "trackSourceServers": [
+    "//higlass.io/api/v1"
+  ],
+  "exportViewUrl": "/api/v1/viewconfs",
+  "views": [
+    {
+      "initialXDomain": [
+        2708428274.52181,
+        2708679235.8772078
+      ],
+      "initialYDomain": [
+        1767819729.5564494,
+        1768368486.6046958
+      ],
+      "tracks": {
+        "top": [
+          {
+            "type": "horizontal-stacked-bar",
+            "tilesetUid": "GIoMpzT4RlOQZ2_7m394Uw",
+            "server": "http://resgen.io/api/v1",
+            "height": 200,
+            "width": 470,
+            "options": {
+              "labelPosition": "topLeft",
+              "labelColor": "black",
+              "labelTextOpacity": 0.4,
+              "valueScaling": "exponential",
+              "trackBorderWidth": 0,
+              "trackBorderColor": "black",
+              "heatmapValueScaling": "log",
+              "name": "all.KL.bed.multires.mv5",
+              "scaledHeight": false,
+              "backgroundColor": "white",
+              "sortLargestOnTop": true,
+              "colorScale": [
+                "#FF0000",
+                "#FF4500",
+                "#32CD32",
+                "#008000",
+                "#006400",
+                "#C2E105",
+                "#FFFF00",
+                "#66CDAA",
+                "#8A91D0",
+                "#CD5C5C",
+                "#E9967A",
+                "#BDB76B",
+                "#808080",
+                "#C0C0C0",
+                "#075eea"
+              ],
+              "barBorder": true
+            },
+            "position": "top",
+            "uid": "Wf5wXtmISKiszukAdGuCsw",
+            "name": "Epilogos (hg19)",
+            "resolutions": [
+              13107200,
+              6553600,
+              3276800,
+              1638400,
+              819200,
+              409600,
+              204800,
+              102400,
+              51200,
+              25600,
+              12800,
+              6400,
+              3200,
+              1600,
+              800,
+              400,
+              200
+            ]
+          }
+        ],
+        "left": [],
+        "center": [],
+        "bottom": [],
+        "right": [],
+        "whole": [],
+        "gallery": []
+      },
+      "layout": {
+        "w": 12,
+        "h": 6,
+        "x": 0,
+        "y": 0,
+        "i": "MiApsjfbQTeRT02rLYDFYQ",
+        "moved": false,
+        "static": false
+      },
+      "uid": "MiApsjfbQTeRT02rLYDFYQ",
+      "genomePositionSearchBoxVisible": true,
+      "genomePositionSearchBox": {
+        "autocompleteServer": "//higlass.io/api/v1",
+        "chromInfoServer": "http://higlass.io/api/v1",
+        "visible": true,
+        "chromInfoId": "hg38",
+        "autocompleteId": "P0PLbQMwTYGy-5uPIQid7A"
+      }
+    }
+  ],
+  "zoomLocks": {
+    "locksByViewUid": {},
+    "locksDict": {}
+  },
+  "locationLocks": {
+    "locksByViewUid": {},
+    "locksDict": {}
+  },
+  "valueScaleLocks": {
+    "locksByViewUid": {},
+    "locksDict": {}
+  }
+};
+
+describe('Test HiGlass Component', () => {
+  let hgc = null;
+  let div = null;
+
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 7000;
+
+  describe('Gene Annotations Overlaps', () => {
+    it('Cleans up previously created instances and mounts a new component', (done) => {
+      if (hgc) {
+        hgc.unmount();
+        hgc.detach();
+      }
+
+      if (div) {
+        global.document.body.removeChild(div);
+      }
+
+      div = global.document.createElement('div');
+      global.document.body.appendChild(div);
+
+      div.setAttribute('style', 'width:800px;background-color: lightgreen');
+      div.setAttribute('id', 'simple-hg-component');
+
+      hgc = mount(<HiGlassComponent
+        options={{ bounded: false }}
+        viewConfig={viewconf}
+      />, { attachTo: div });
+
+      hgc.update();
+      waitForTilesLoaded(hgc.instance(), done);
+    });
+
+    it('Check that the component is rendered correctly', (done) => {
+      done();
+    });
+  });
+});
+
