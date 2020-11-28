30c265174c24db40492df03c159982a6d5a3881f
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2020-11-28 17:55:33 -0500

    Support tooltip and add tests

--- test/StackedBarTests.js
+++ test/StackedBarTests.js
@@ -16,6 +16,7 @@ import {
 import StackedBarTrack from '../src/scripts/StackedBarTrack';
 
 import viewConf from './view-configs/states-data';
+import viewConfSelectRows from './view-configs/select-rows';
 
 
 register({
@@ -38,6 +39,27 @@ describe('Check bar track with states data', () => {
         expect(trackObj.maxAndMin.max).to.be.eql(4);
         expect(trackObj.maxAndMin.min).to.be.eql(0);
 
+        done();
+      });
+      
+  });
+
+  it('Ensures that the selectRows option is correctly processed', (done) => {
+
+    const [div, hgc] = mountHGComponentLocalTiles(null, null, viewConfSelectRows, 
+      
+      () => {
+        const trackObj = getTrackObjectFromHGC(hgc.instance(), 'view-uid', 'track-uid');
+        
+        expect(trackObj.numCategories).to.be.eql(5);
+
+        const matrix = trackObj.simpleUnFlatten(
+          { tileData: { shape: [15, 1] } },
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        )
+        // selectRows: [[14, 1], [2, 3], [4, 5], [6, 7, 8, 9, 10, 11, 12, 13], [0]]
+        expect(matrix).to.be.eql([[15, 5, 9, 76, 0]]);
+        
         done();
       });
       
